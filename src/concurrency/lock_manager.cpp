7c2599252927871fd486f9eb9d96e210e1fa40cc
hash-roar <1022647354@qq.com>
2022-10-20 13:52:32 +0000

    :construction: (log manager)

--- src/concurrency/lock_manager.cpp
+++ src/concurrency/lock_manager.cpp
@@ -12,18 +12,54 @@
 
 #include "concurrency/lock_manager.h"
 
+#include <algorithm>
+#include <memory>
+#include <mutex>
 #include <utility>
 #include <vector>
+#include "concurrency/transaction.h"
 
 namespace bustub {
 
 bool LockManager::LockShared(Transaction *txn, const RID &rid) {
-  txn->GetSharedLockSet()->emplace(rid);
+  // verify valid
+  if (txn->GetState() == TransactionState::ABORTED || txn->GetIsolationLevel() == IsolationLevel::READ_UNCOMMITTED ||
+      txn->IsSharedLocked(rid)) {
+    txn->SetState(TransactionState::ABORTED);
+    return false;
+  }
+  auto &lque = lock_table_[rid];
+
+  // for that other thread may change the request list,we use iter here
+
+  auto iter = lque.request_queue_.begin();
+
+  while (iter != lque.request_queue_.end()) {
+  }
+
   return true;
 }
 
 bool LockManager::LockExclusive(Transaction *txn, const RID &rid) {
+  std::unique_lock lock{latch_};
   txn->GetExclusiveLockSet()->emplace(rid);
+  auto &lque = lock_table_[rid];
+  if (txn->GetState() == TransactionState::ABORTED) {
+    return false;
+  }
+  if (lock_table_.count(rid) == 0) {
+    lock_table_[rid].request_queue_.emplace_back(txn->GetTransactionId(), LockMode::EXCLUSIVE, true);
+  } else {
+    auto &que = lock_table_[rid].request_queue_;
+
+    bool has_lock = std::any_of(que.begin(), que.end(), [](const LockRequest &req) -> bool { return req.granted_; });
+    if (has_lock) {
+      lock_table_[rid].cv_.wait(lock);
+      // get lock now
+      lock_table_[rid].request_queue_.emplace_back(txn->GetTransactionId(), LockMode::EXCLUSIVE, true);
+    }
+  }
+
   return true;
 }
 
